f1abc1c8aa79c077fd68ed3affcd107648686a5f
Daichi Aida <da1649ichi@gmail.com>
2024-11-29 17:02:08 +0900

    Add a Test

--- test.bash
+++ test.bash
@@ -9,16 +9,14 @@ ng () {
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+out=$(echo "aida" | ./alphabet)
+[ "${out}" = "変換結果: AIDA" ] || ng "$LINENO"
 
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+out=$(echo "あ" | ./alphabet)
+[ "${out}" = "変換結果: あ" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+out=$(echo "1./:" | ./alphabet)
+[ "${out}" = "変換結果: 1./:" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 exit $res

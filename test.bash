c178da877be5d54b2c770ab8e8d6d78ff832f8c0
Daichi Aida <da1649ichi@gmail.com>
2024-11-29 19:32:46 +0900

    Add command

--- test.bash
+++ test.bash
@@ -13,10 +13,12 @@ out=$(echo "aida" | ./alphabet)
 [ "${out}" = "変換結果: AIDA" ] || ng "$LINENO"
 
 out=$(echo "あ" | ./alphabet)
-[ "${out}" = "変換結果: あ" ] || ng "$LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo "1./:" | ./alphabet)
-[ "${out}" = "変換結果: 1./:" ] || ng "$LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 exit $res
